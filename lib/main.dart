09b50926aadfe19e2472c827e59b1cc10a49f3b2
A.rahman <mrahman171306@bscse.uiu.ac.bd>
2022-06-26 23:59:18 +0600

    Responsive design added

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'content_page.dart';
 import 'my_detail_page.dart';
 import 'my_home_page.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,6 +14,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      builder: (context, child) => ResponsiveWrapper.builder(
+        BouncingScrollWrapper.builder(context, child!),
+        maxWidth: 1200,
+        minWidth: 450,
+        defaultScale: true,
+        breakpoints: [
+          const ResponsiveBreakpoint.resize(450, name: MOBILE),
+          const ResponsiveBreakpoint.autoScale(800, name: TABLET),
+          const ResponsiveBreakpoint.autoScale(1000, name: TABLET),
+          const ResponsiveBreakpoint.resize(1200, name: DESKTOP),
+          const ResponsiveBreakpoint.autoScale(2460, name: "4K"),
+        ],
+        background: Container(
+          color: const Color(0xFFF5F5F5),
+        ),
+      ),
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
